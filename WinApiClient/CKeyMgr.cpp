d56f0bd4467df0fede96fdd72cfbeaaeb110d194
gghtn <ghtn9898@gmail.com>
2022-11-16 23:58:34 +0900

    keymgr

--- WinApiClient/CKeyMgr.cpp
+++ WinApiClient/CKeyMgr.cpp
@@ -1,2 +1,73 @@
 #include "pch.h"
 #include "CKeyMgr.h"
+
+int g_arrVK[(int)KEY::LAST]	// KEY에 대응하는 실제 값
+{
+	VK_LEFT,
+	VK_RIGHT,
+	VK_UP,
+	VK_DOWN,
+	VK_MENU,
+	VK_LSHIFT,
+	VK_SPACE,
+	VK_RETURN,
+	VK_ESCAPE,
+	'A',
+	'S',
+	'D',
+	'F',
+	'Z',
+	'X',
+	'C',
+	'V'
+};
+
+CKeyMgr::CKeyMgr()
+{ 
+
+}
+
+CKeyMgr::~CKeyMgr()
+{
+
+}
+
+void CKeyMgr::Init()
+{
+	for (int i = 0; i < (int)KEY::LAST; i++) {
+		m_vecKey.push_back(KEY_INFO{KEY_STATE::NONE, false});
+	}
+}
+
+void CKeyMgr::Update()
+{
+	for (int i = 0; i < (int)KEY::LAST; i++) {
+		
+		if (GetAsyncKeyState(g_arrVK[i])& 0x8000) // 키가 눌렸을 때
+		{
+			if (m_vecKey[i].bPrevPush)	// 이전에 눌렸으면
+			{
+				m_vecKey[i].eState = KEY_STATE::HOLD;
+			}
+			else                       // 이전에 안눌렸으면
+			{
+				m_vecKey[i].eState = KEY_STATE::TAP;
+			}
+
+			m_vecKey[i].bPrevPush = true;
+		}
+		else                            // 키가 안눌렸을 때
+		{
+			if (m_vecKey[i].bPrevPush)	// 이전에 눌렸으면
+			{
+				m_vecKey[i].eState = KEY_STATE::AWAY;
+			}
+			else                        // 이전에 안눌렸으면
+			{
+				m_vecKey[i].eState = KEY_STATE::NONE;
+			}
+			m_vecKey[i].bPrevPush = false;
+		}
+	}
+
+}
--- WinApiClient/CKeyMgr.h
+++ WinApiClient/CKeyMgr.h
@@ -1,7 +1,52 @@
 #pragma once
+
+enum class KEY_STATE
+{
+	NONE,	// 눌리지 않았고, 이전에도 눌리지 않은 상태
+	TAP,	// 처음 누른 시점
+	HOLD,	// 누르그 있는 상태
+	AWAY,	// 키를 뗀 시점
+};
+
+enum class KEY
+{
+	LEFT,
+	RIGHT,
+	UP,
+	DOWN,
+	ALT,
+	LSHIFT,
+	SPACE,
+	ENTER,
+	ESC,
+	A,
+	S,
+	D,
+	F,
+	Z,
+	X,
+	C,
+	V,
+
+	LAST //enum의 끝
+};
+
+struct KEY_INFO
+{
+	KEY_STATE	eState;
+	bool		bPrevPush;	// 이전 프레임에서 눌렸는지
+};
+
 class CKeyMgr
 {
-public:
 	SINGLE(CKeyMgr);
+private:
+	vector<KEY_INFO> m_vecKey;
+
+public:
+	void Init();
+	void Update();
+
+	KEY_STATE GetKeyState(KEY eKey) { return m_vecKey[(int)eKey].eState; }
 };
 
