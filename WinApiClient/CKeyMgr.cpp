31e846bbc035d74d9e352215899cb10f1987fde6
gghtn <ghtn9898@gmail.com>
2022-11-17 23:38:24 +0900

    KeyMgr focusing 추가

--- WinApiClient/CKeyMgr.cpp
+++ WinApiClient/CKeyMgr.cpp
@@ -1,6 +1,8 @@
 #include "pch.h"
 #include "CKeyMgr.h"
 
+#include "CCore.h"
+
 int g_arrVK[(int)KEY::LAST]	// KEY에 대응하는 실제 값
 {
 	VK_LEFT,
@@ -41,33 +43,60 @@ void CKeyMgr::Init()
 
 void CKeyMgr::Update()
 {
-	for (int i = 0; i < (int)KEY::LAST; i++) {
-		
-		if (GetAsyncKeyState(g_arrVK[i])& 0x8000) // 키가 눌렸을 때
-		{
-			if (m_vecKey[i].bPrevPush)	// 이전에 눌렸으면
-			{
-				m_vecKey[i].eState = KEY_STATE::HOLD;
-			}
-			else                       // 이전에 안눌렸으면
-			{
-				m_vecKey[i].eState = KEY_STATE::TAP;
-			}
 
-			m_vecKey[i].bPrevPush = true;
-		}
-		else                            // 키가 안눌렸을 때
-		{
-			if (m_vecKey[i].bPrevPush)	// 이전에 눌렸으면
+	//윈도우 포커싱 알아내기
+	//HWND hMainWnd = CCore::GetInstance()->GetMainHwnd();
+	HWND hWnd = GetFocus();
+
+	// 현재 윈도우가 포커싱 중일 때
+	if(hWnd != nullptr)
+	{
+		for (int i = 0; i < (int)KEY::LAST; i++) {
+
+			if (GetAsyncKeyState(g_arrVK[i]) & 0x8000) // 키가 눌렸을 때
 			{
-				m_vecKey[i].eState = KEY_STATE::AWAY;
+				if (m_vecKey[i].bPrevPush)	// 이전에 눌렸으면
+				{
+					m_vecKey[i].eState = KEY_STATE::HOLD;
+				}
+				else                       // 이전에 안눌렸으면
+				{
+					m_vecKey[i].eState = KEY_STATE::TAP;
+				}
+
+				m_vecKey[i].bPrevPush = true;
 			}
-			else                        // 이전에 안눌렸으면
+			else                            // 키가 안눌렸을 때
 			{
-				m_vecKey[i].eState = KEY_STATE::NONE;
+				if (m_vecKey[i].bPrevPush)	// 이전에 눌렸으면
+				{
+					m_vecKey[i].eState = KEY_STATE::AWAY;
+				}
+				else                        // 이전에 안눌렸으면
+				{
+					m_vecKey[i].eState = KEY_STATE::NONE;
+				}
+				m_vecKey[i].bPrevPush = false;
 			}
-			m_vecKey[i].bPrevPush = false;
 		}
 	}
+	// 포커싱 해제 상태 (창이 내려가 있을 때 키를 입력해도 움직이지 않도록)
+	else
+	{
+		for (int i = 0; i < (int)KEY::LAST; i++)
+		{
+			m_vecKey[i].bPrevPush = false;
+
+			if (m_vecKey[i].eState == KEY_STATE::AWAY)
+			{
+				m_vecKey[i].eState == KEY_STATE::NONE;
+			}
+			else if (m_vecKey[i].eState == KEY_STATE::TAP || m_vecKey[i].eState == KEY_STATE::HOLD)
+			{
+				m_vecKey[i].eState == KEY_STATE::AWAY;
+			}
+		}
+	}
+	
 
 }
